c14c48a57993b26f5deebb4a29eade999e6248b2
Ivan Demidov <Scrum@list.ru>
2020-11-26 18:04:09 +0300

    style: temp ignore because need refactor types in posthtml

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 'use strict';
 
-import {Node} from 'posthtml';
-import parser from 'posthtml-parser';
+import {Node, Options as PosthtmlOptions} from 'posthtml';
+import parser, {Options as ParserOptions} from 'posthtml-parser';
+
+interface Options extends PosthtmlOptions, ParserOptions {}
 
 const posthtmlParserTitle = () => (tree: Node): Node => {
   const {options} = tree;
 
-  return tree.match({tag: 'title'}, (node: Node): Node => {
-    node.content = node.content.map(content => parser(content, options));
+  tree.match({tag: 'title'}, (node: Node): Node => {
+    if (node.content) {
+      // @ts-expect-error
+      node.content = node.content.flatMap(content => {
+        if (typeof content === 'string') {
+          return parser(content, options);
+        }
+
+        return node;
+      });
+    }
 
     return node;
   });
+
+  return tree;
 };
 
 export default posthtmlParserTitle;
