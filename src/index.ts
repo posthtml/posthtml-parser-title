7f491c78997d83e397ee164dfd9be36a062d8794
Ivan Demidov <Scrum@list.ru>
2020-11-27 09:55:10 +0300

    fix: tmp ignore type options from posthtml

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 'use strict';
 
-import {Node, Options as PosthtmlOptions} from 'posthtml';
-import parser, {Options as ParserOptions} from 'posthtml-parser';
-
-interface Options extends PosthtmlOptions, ParserOptions {}
+import {Node} from 'posthtml';
+import parser from 'posthtml-parser';
 
 const posthtmlParserTitle = () => (tree: Node): Node => {
   const {options} = tree;
@@ -13,6 +11,7 @@ const posthtmlParserTitle = () => (tree: Node): Node => {
       // @ts-expect-error
       node.content = node.content.map(content => {
         if (typeof content === 'string') {
+          // @ts-expect-error
           const [node] = parser(content, options);
           return node;
         }
